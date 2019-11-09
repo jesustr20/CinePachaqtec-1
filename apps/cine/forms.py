35e8eb0ec8f44653b307e8ba2c7f32d56f1b8564
u201014550 <47009336+u201014550@users.noreply.github.com>
2019-11-09 17:44:57 -0500

    Todas los modelos

--- apps/cine/forms.py
+++ apps/cine/forms.py
@@ -4,4 +4,4 @@ from .models import Local
 class LocalForm(forms.ModelForm):
     class Meta:
         model = Local
-        fields = {'nombre', 'direccion', 'sevicio', 'distrito'}
+        fields = {'nombre', 'direccion', 'tiposervicio', 'precio','distrito','imagen'}
